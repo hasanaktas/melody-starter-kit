d5831d3f1eee4ee065628ff0c97cd64b192a3ef2
hasanaktas <developer@hasanaktas.com>
2022-09-28 19:44:04 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-pink-500 flex min-h-screen justify-center items-center">
+      <h6 className="text-white text-xl">Melody Starter Kit</h6>
+    </div>
+  );
+};
+
+export default App;
